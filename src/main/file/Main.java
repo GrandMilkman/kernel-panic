a11b49af597c8aa865f73600aba0e1789d6fc180
mikita <mikita@mikita>
2019-09-21 15:45:51 +0300

    new project

---
+++ src/main/file/Main.java
@@ -0,0 +1,11 @@
+package file;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class Main extends SpringBootServletInitializer {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+}
